182b19cd5f1e99314681f87156b4076d63e432b0
Aayushi sharma <104767330+Aayushi-birdvision@users.noreply.github.com>
2022-05-10 17:04:19 +0530

    Update DynamicTable.js

--- DynamicTable.js
+++ DynamicTable.js
@@ -4,39 +4,33 @@ import "./table.css"
 
 function DynamicTable(){
  const column = Object.keys(TableData[0]);
- 
  const ThData =()=>{   
-  return column.map((data)=>{
-  return <th key={data}>{data}</th>
-     })
+ eturn column.map((data)=>{
+ return <th key={data}>{data}</th>
+ })
  }
 const tdData =() =>{
-   
- return TableData.map((data)=>{
-   return(
-     <tr>
-       {
-           column.map((v)=>{
-           return <td>{data[v]}</td>
-                   })
-                }
-           </tr>
+return TableData.map((data)=>{
+ return(
+  <tr>
+    {	
+      column.map((v)=>{
+       return <td>{data[v]}</td>
+     })
+       }
+    </tr>
        )
      })
 }
-  return (
-    
-      <table className="table">
-        <thead>
-         <tr>{ThData()}</tr>
-        </thead>
-        <tbody>
+return (
+   <table className="table">
+     <thead>
+        <tr>{ThData()}</tr>
+      </thead>
+      <tbody>
         {tdData()}
-        </tbody>
-       </table>
-    
+      </tbody>
+      </table>  
   )
-
-  
 }
 export default DynamicTable;
