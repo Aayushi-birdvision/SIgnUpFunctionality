336b4541ba5fe3178f1482c02342627fedf4912b
Aayushi sharma <104767330+Aayushi-birdvision@users.noreply.github.com>
2022-05-13 14:05:03 +0530

    recent changes

--- src/component/User.js
+++ src/component/User.js
@@ -1,79 +1,76 @@
 
-import React, { useState , Component } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import "./users.css"
 
 export default class Users extends React.Component {
-  state = {
-    const: [loading, setLoading]=useState(true);
-    const: [userData,setUserData]=useState(null);
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      loading: true,
+    }
   }
   mount = async () => {
-    try{
     let res = await axios
-    .get(`https://jsonplaceholder.typicode.com/users`)
+      .get(`https://jsonplaceholder.typicode.com/users`)
     const users = res.data;
-    this.setState({ users });
-    setUserData(res.data)
-    setLoading(false);
+    this.setState({ loading: false });
   }
-  catch(e){
-    console.log(e)
-  }}
   componentDidMount() {
     this.mount();
   }
-  useEffect(  ()=> {
-   mount()
-  },[]
-  )
-    return <div className="table">Loading...</div>;
-  }
   render() {
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <td>ID</td>
-            <td>NAME</td>
-            <td>USERNAME</td>
-            <td>STREET</td>
-            <td>SUITE</td>
-            <td>CITY</td>
-            <td>ZIPCODE</td>
-            <td>LAT</td>
-            <td>LNG</td>
-            <td>PHONE</td>
-            <td>WEBSITE</td>
-            <td>NAME</td>
-            <td>CATCHPHRASE</td>
-            <td>BS</td>
-          </tr>
-        </thead>
-        <tbody>{
-          this.state.users.map(
-            user => <>
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.name}</td>
-                <td>{user.username}</td>
-                <td>{user.address.street}</td>
-                <td>{user.address.suite}</td>
-                <td>{user.address.city}</td>
-                <td>{user.address.zipcode}</td>
-                <td>{user.address.geo.lat}</td>
-                <td>{user.address.geo.lng}</td>
-                <td>{user.phone}</td>
-                <td>{user.website}</td>
-                <td>{user.company.name}</td>
-                <td>{user.company.catchPhrase}</td>
-                <td>{user.company.bs}</td>
-              </tr>
-            </>
-          )
-        }</tbody>
-      </table>
-    );
-  }
+    if (this.state.loading) {
+      return (<h1>"Loading"</h1>)
+    } else {
+      return (
+        <table className="table">
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>NAME</td>
+              <td>USERNAME</td>
+              <td>STREET</td>
+              <td>SUITE</td>
+              <td>CITY</td>
+              <td>ZIPCODE</td>
+              <td>LAT</td>
+              <td>LNG</td>
+              <td>PHONE</td>
+              <td>WEBSITE</td>
+              <td>NAME</td>
+              <td>CATCHPHRASE</td>
+              <td>BS</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.users.map(
+               user => <>
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.address.street}</td>
+                  <td>{user.address.suite}</td>
+                  <td>{user.address.city}</td>
+                  <td>{user.address.zipcode}</td>
+                  <td>{user.address.geo.lat}</td>
+                  <td>{user.address.geo.lng}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.website}</td>
+                  <td>{user.company.name}</td>
+                  <td>{user.company.catchPhrase}</td>
+                  <td>{user.company.bs}</td>
+                </tr>
+               </>
+              )
+            }
+          </tbody>
+        </table>
+      )
+    }
 
+  }
 }
